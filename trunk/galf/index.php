fb999087efc55a7fa7af7003e7582f85ccfaef7d
epere4 <epere4@54a3cb9f-b05d-4279-8433-ee0e25a2a64a>
2013-01-23 18:53:05 +0000

    First version of the site. This is not the original site. The first one was modified by the creator, then Agustin did some extra modifications. This is the first versioned version.

---
+++ trunk/galf/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>PEREDA AGRO: GALERIA DE FOTOS</title>
+<link rel="stylesheet" type="text/css" href="../rsrc/txt.css">
+<link rel="stylesheet" type="text/css" href="../rsrc/screen.css">
+<link rel="stylesheet" href="style.css" />
+<script type="text/javascript" src="../rsrc/js/most.js"></script>
+</head>
+<body>
+<div id="MainBody">
+<div id="MainContent" style=" max-height:700px;"><div id="engl"><a href="../eng/home/">English</a></div>
+	<div id="Menu">
+	 <a href="../home/"><div id="Logo"></div></a>
+	  <li class="vde"><a href="../home/index.html">Home</a></li>
+	  <li class="vde"><a href="../empr/index.html">Empresa</a></li>
+	  <li class="vde" id="pr"><a href="#" onclick="oculta('1');">Productos</a></li>
+	  <li class="prd" id="prd" style="display:none;"><a href="../produ/index.html">Girasol Confitero</a></li>
+	  <li class="prd" id="prd2" style="display:none;"><a href="../produ/ceba.html">Cebada</a></li>
+	  <li class="prd2" id="prd3" style="display:none;"><a href="../produ/pop.html">Pop-Corn</a></li>
+	  <li class="vde2"><a href="../expo/index.html">Exportaci&oacute;n</a></li>
+	 <div id="Menures">
+	 <li class="grs"><a href="../lnkn/index.html">Link de inter&eacute;s</a></li>
+	 <li class="grs"><a href="../galf/index.html" style="color:#ECA800;">Galer&iacute;a de fotos</a></li>
+	 <li class="grs"><a href="../cntc/index.html">Contacto</a></li>
+	 </div><!--end Menures-->
+	</div><!--end Menu-->
+	<div id="Conten">
+<ul id="slideshow">
+ 		<li>
+	<!--1--><h3></h3><span>photos/p1.jpg</span><p></p><a href="#"><img src="thumbnails/p1.jpg" alt="Pereda 1"/></a>
+		</li>
+		<li>
+	<!--2--><h3></h3><span>photos/p2.jpg</span><p></p><a href="#"><img src="thumbnails/p2.jpg" alt="Pereda 2"/></a>
+		</li>
+		<li>
+	<!--3--><h3></h3><span>photos/p3.jpg</span><p></p><a href="#"><img src="thumbnails/p3.jpg" alt="Pereda 3"/></a>
+		</li>
+		<li>
+	<!--4--><h3></h3><span>photos/p4.jpg</span><p></p><a href="#"><img src="thumbnails/p4.jpg" alt="Pereda 4"/></a>
+		</li>
+		<li>
+	<!--5--><h3></h3><span>photos/p5.jpg</span><p></p><a href="#"><img src="thumbnails/p5.jpg" alt="Pereda 5"/></a>
+		</li>
+</ul>
+ <div id="wrapper" >
+  <div id="fullsize">
+	<div id="imgprev" class="imgnav" title="Previous Image"></div>
+	  <div id="imglink"></div>
+		<div id="imgnext" class="imgnav" title="Next Image"></div>
+		  <div id="image"></div>
+	<div id="information">
+	 </div>
+ </div>
+ <div id="thumbnails" >
+  <div id="slideleft" title="Slide Left"></div>
+   <div id="slidearea">
+	<div id="slider"></div>
+   </div>
+	<div id="slideright" title="Slide Right"></div>
+ </div>
+</div>  
+<script type="text/javascript" src="compressed.js"></script>
+<script type="text/javascript">
+	$('slideshow').style.display='none';
+	$('wrapper').style.display='block';
+	var slideshow=new TINY.slideshow("slideshow");
+	window.onload=function(){
+		slideshow.auto=true;
+		slideshow.speed=5;
+		slideshow.link="linkhover";
+		slideshow.info="information";
+		slideshow.thumbs="slider";
+		slideshow.left="slideleft";
+		slideshow.right="slideright";
+		slideshow.scrollSpeed=4;
+		slideshow.spacing=5;
+		slideshow.active="#fff";
+		slideshow.init("slideshow","image","imgprev","imgnext","imglink");
+	}
+</script>
+</div>
+</div>
+<div id="socig">
+        <h3>Compartir en: <a href="http://www.facebook.com/sharer.php?u=http://www.peredaagro.com/" target="_blank"><img src="../rsrc/facebook.png" border="0"/></a><a href="http://twitter.com/home?status=http://www.peredaagro.com/" target="_blank"><img src="../rsrc/twitter.png" style="margin-left:10px; border:0;"/></a><a href="https://plus.google.com/share?url=http://www.peredaagro.com/" target="_blank" style="text-decoration:none;"><img src="../rsrc/google.png" style="margin-left:10px; border:0;"/></a></h3>
+      </div>
+	  <div id="footg">
+        <h3>Copyright &copy; 2012 Pereda Agro S.A. Reservados todos los derechos.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="../home/index.html">home</a> | <a href="../mapa/index.php">mapa de sitio</a> | <a href="../cntc/index.html">contacto</a></h3></div>
+
+	</div>
+</div>
+</body>
+</html>
+
