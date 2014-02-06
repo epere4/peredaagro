c86e88ebd5412d7ec437a1646d974809fe568095
epere4 <epere4@gmail.com>
2014-02-06 22:43:56 +0000

    Banner for Gulfood (Dubai) 2014. Plus, the configuration was extracted to a /banner folder, that has a banner.jpg file and a banner.js. So, next time, that is the only thing that we will need to modify.

---
+++ banner/banner.js
@@ -0,0 +1,33 @@
+
+function createDate(dateConfig) {
+  return new Date(dateConfig.year, dateConfig.month - 1, dateConfig.day, 0, 0, 0, 0);
+}
+
+$(document).ready(function(){	
+  var start = {
+    year: 2014,
+    month: 2,
+    day: 1
+  },
+  end = {
+    year: 2014,
+    month: 3,
+    day: 15
+  },
+  link = "http://www.gulfood.com/Exhibitor/Pereda-Agro-S.A.";
+  
+  var now = new Date();
+  var startTimeStamp = createDate(start);
+  var endTimeStamp = createDate(end);
+  
+  console.log("Now is              ", now);
+  console.log("Banner running from ", startTimeStamp);
+  console.log("Banner running until", endTimeStamp);
+  
+  if (startTimeStamp.getTime() <= now.getTime() && now.getTime() <= endTimeStamp.getTime()) {
+    console.log($("#Banner a"));
+    console.log(link);
+    $("#Banner a").attr("href", link);
+    $("#Banner").show();
+  }
+});
